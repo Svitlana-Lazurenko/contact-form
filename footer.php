fc1f113d0a761d2e6ce37b282778f0d5c40b6c6d
Svitlana Lazurenko <lazurenko.svitlana.v@gmail.com>
2025-02-16 22:22:59 +0200

    Add modal html-markup

--- footer.php
+++ footer.php
@@ -5,7 +5,18 @@ if (!defined('ABSPATH')) exit; ?>
 </footer>
 
 
-
+<div class="modal">
+    <div class="modal__content">
+        <button class="close-btn"><span class="close-btn__text">Закрити</span>
+            <div class="close-btn__icon"></div>
+        </button>
+        <div class="modal__result">
+            <div class="modal__img"></div>
+            <p class="modal__text">Ваш запит надіслано</p>
+        </div>
+        <p class="modal__gratitude">Дякуємо, що довіряєте!</p>
+    </div>
+</div>
 
 
 <?php wp_footer(); ?>
